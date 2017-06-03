f79a39e01105f2fbf7cd81258d470ffdb3c0af8c
shefin-stevenson <shefin@gmail.com>
2017-06-03 10:04:43 -0400

    Update test.c
    added whitespace and modified output string.

--- source/test.c
+++ source/test.c
@@ -5,9 +5,12 @@
 int main()
 {
   char name[10];
+  
   printf("Who are you? ");
   fgets(name,10,stdin);
-  printf("Glad to meet you, %s.\n",name);
+  
+  printf("I am glad to meet you, %s.\n",name);
+  
   return(0);
 }
 
